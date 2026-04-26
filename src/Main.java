e9e6d9c85337fa05fd021d28d7ccaf031c15950f
llNestyll <llnestyll@ro.ru>
2026-04-26 19:35:49 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+
+        int luck = 273;
+        System.out.println("Значение переменной  luck с типом int равно" + luck);
+        byte rang = 10;
+        System.out.println("Значение переменной  rang с типом byte равно" + rang);
+        short yuo = 30052;
+        System.out.println("Значение переменной  you с типом short равно" + yuo);
+        long duo = 90019838;
+        System.out.println("Значение переменной  duo с типом long равно" + duo);
+        float raf = 2.312F;
+        System.out.println("Значение переменной  raf с типом float равно" + raf);
+double hub = 1.00000011;
+        System.out.println("Значение переменной  hub с типом double равно" + hub);
+
+    }
+}
